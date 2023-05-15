c3f8662946606b5b042db552e53fd5ff83c76022
jenil-simform <jenil@simformsolutions.com>
2023-05-15 12:05:21 +0530

    serialization using custom interceptor with DTO and custom Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,13 +7,12 @@ import {
   Patch,
   Delete,
   Query,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -24,7 +23,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     console.log('handler is running!');
